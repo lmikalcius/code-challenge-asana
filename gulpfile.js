fc68ba91358d40ddb200875f69f0748e66234509
linas1985 <linas.mikalcius@gmail.com>
2018-03-12 18:23:35 -0500

    Add base HTML, CSS modules, and import plugin

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var cssVars = require("postcss-simple-vars");
 var nested = require("postcss-nested");
+var cssImport = require("postcss-import");
 
 gulp.task("default", function () {
   console.log("Sample Gulp Task");
@@ -16,8 +17,8 @@ gulp.task("html", function () {
 gulp.task("styles", function () {
   // pipe from app and create temp folder for output CSS
   return gulp.src("./app/assets/styles/styles.css")
-  // add for CSS webkits, vars, nested CSS
-  .pipe(postcss([cssVars, nested, autoprefixer]))
+  // add for CSS webkits, vars, nested CSS, importing files so CSS doesn't have to make different calls
+  .pipe(postcss([cssImport, cssVars, nested, autoprefixer]))
   .pipe(gulp.dest("./app/temp/styles"));
 });
 
