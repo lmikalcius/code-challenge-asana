24c87a15cbc4f2cc5d9aef3da4cd3247196c3ce4
linas1985 <linas.mikalcius@gmail.com>
2018-03-11 23:52:39 -0500

    Add gulp piping for CSS along with basic SASS plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require("gulp");
 var watch = require("gulp-watch");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var cssVars = require("postcss-simple-vars");
+var nested = require("postcss-nested");
 
 gulp.task("default", function () {
   console.log("Sample Gulp Task");
@@ -10,7 +14,11 @@ gulp.task("html", function () {
 });
 
 gulp.task("styles", function () {
-  console.log("Sample CSS Task");
+  // pipe from app and create temp folder for output CSS
+  return gulp.src("./app/assets/styles/styles.css")
+  // add for CSS webkits, vars, nested CSS
+  .pipe(postcss([cssVars, nested, autoprefixer]))
+  .pipe(gulp.dest("./app/temp/styles"));
 });
 
 gulp.task("watch", function () {
