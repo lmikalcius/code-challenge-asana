42578b08775b49031dd0f20d86169aab2e910908
linas1985 <linas.mikalcius@gmail.com>
2018-03-12 19:33:20 -0500

    Add mixins

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,13 +5,14 @@ var autoprefixer = require("autoprefixer");
 var cssVars = require("postcss-simple-vars");
 var nested = require("postcss-nested");
 var cssImport = require("postcss-import");
+var mixins = require("postcss-mixins");
 
 
 gulp.task("styles", function () {
   return gulp.src("./app/assets/styles/styles.css")
 
   // add for CSS webkits, vars, nested CSS, importing files so CSS doesn't have to make different calls
-  .pipe(postcss([cssImport, cssVars, nested, autoprefixer]))
+  .pipe(postcss([cssImport, mixins, cssVars, nested, autoprefixer]))
 
   // add error handling so watch doesn't stop
   .on("error", function (errorInfo) {
