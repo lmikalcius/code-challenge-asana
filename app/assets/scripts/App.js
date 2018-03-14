6b0d56850d6454b0dcd1128b37ad8341cdd2cca5
linas1985 <linas.mikalcius@gmail.com>
2018-03-14 00:33:29 -0500

    Add CSS/style file structure and initial JS for scroll reveal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -11,6 +11,50 @@ if (projectNumber) {
                     "redirect_uri=https://myapp.com/oauth&" +
                     "response_type=token&" +
                     "state=projectNumber"
+} else {
+  // new model
+  // new view
+}
+
+
+
+
+
+
+window.onload = function() {
+
+  var section = document.getElementsByClassName('tasks')[0];
+  // console.log(section);
+  // console.log(document.body);
+  // document.body.appendChild(section);
+  // for (let x = 1; x <= 55; x ++) {
+  //   let d = document.createElement('div');
+  //   d.innerText = `Hello there div line ${x} of 55`;
+  //   if (x > 3 && x < 25) { d.className += " ptask"; }
+  //   d.className += " task";
+  //   section.appendChild(d);
+  // }
+
+  function inView (el) {
+    var sb = section.getBoundingClientRect();
+    var eb = el.getBoundingClientRect();
+    return eb.top < sb.height;
+  }
+
+  function updateInView() {
+    var tasks = document.querySelectorAll('.task');
+
+    for (var i = 0; i < tasks.length; ++i) {
+      if (inView(tasks[i])) {
+        tasks[i].classList.add('task--inview')
+      };
+    }
+  }
+
+  document.onscroll = updateInView;
+
+  updateInView();
+
 }
 
 
--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -85,8 +85,46 @@ console.log((0, _queryParam2.default)("project"));
 
 var projectNumber = (0, _queryParam2.default)("project");
 if (projectNumber) {
-                  window.location = "https://app.asana.com/-/oauth_authorize?" + "client_id=123&" + "redirect_uri=https://myapp.com/oauth&" + "response_type=token&" + "state=projectNumber";
+  window.location = "https://app.asana.com/-/oauth_authorize?" + "client_id=123&" + "redirect_uri=https://myapp.com/oauth&" + "response_type=token&" + "state=projectNumber";
+} else {
+  // new model
+  // new view
 }
+
+window.onload = function () {
+
+  var section = document.getElementsByClassName('tasks')[0];
+  // console.log(section);
+  // console.log(document.body);
+  // document.body.appendChild(section);
+  // for (let x = 1; x <= 55; x ++) {
+  //   let d = document.createElement('div');
+  //   d.innerText = `Hello there div line ${x} of 55`;
+  //   if (x > 3 && x < 25) { d.className += " ptask"; }
+  //   d.className += " task";
+  //   section.appendChild(d);
+  // }
+
+  function inView(el) {
+    var sb = section.getBoundingClientRect();
+    var eb = el.getBoundingClientRect();
+    return eb.top < sb.height;
+  }
+
+  function updateInView() {
+    var tasks = document.querySelectorAll('.task');
+
+    for (var i = 0; i < tasks.length; ++i) {
+      if (inView(tasks[i])) {
+        tasks[i].classList.add('task--inview');
+      };
+    }
+  }
+
+  document.onscroll = updateInView;
+
+  updateInView();
+};
 
 var firstTask = new _Task2.default("Code a lot", 234189);
 console.log(firstTask.task, firstTask.id);
